4912af8accdd8ebd40ee18760b5fd4f0f6dde67a
Nalan Gelir <nalangelir@outlook.com>
2025-05-04 16:02:55 +0300

    connect azure iot central app

---
+++ Azure_IoT_Central_ESP32/system_configs.h
@@ -0,0 +1,12 @@
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+#define OLED_RESET -1
+#define SCREEN_ADDRESS 0x3C
+
+
+#define SOUND_SPEED 0.034
+
+// Sensor Pins
+const int trigPins[3] = {5, 14, 26};
+const int echoPins[3] = {18, 27, 25};
+
